3b1a7fbf75e8569b905569327bc1de5f0fb9522e
Sciss <contact@sciss.de>
2011-10-05 00:10:48 +0100

    if i'm not mistaken, `map` and `flatMap` work as intended.

--- src/main/scala/de/sciss/grapheme/Tests.scala
+++ src/main/scala/de/sciss/grapheme/Tests.scala
@@ -5,10 +5,15 @@ object Tests {
       args.headOption match {
          case Some( "--fut1" ) => future1()
          case Some( "--fut2" ) => future2()
+         case Some( "--fut3" ) => future3()
+         case Some( "--fut4" ) => future4()
          case _ => sys.error( "Illegal arguments : " + args )
       }
    }
 
+   /**
+    * Tests the `apply` method of `FutureResult` when it requires waiting for the result.
+    */
    def future1() {
       import actors.{TIMEOUT, Actor}
       import Actor._
@@ -27,6 +32,9 @@ object Tests {
       }
    }
 
+   /**
+    * Tests the `apply` method of `FutureResult` when it is instantly available.
+    */
    def future2() {
       import actors.{TIMEOUT, Actor}
       import Actor._
@@ -44,6 +52,60 @@ object Tests {
                val res = ev.apply()
                println( "result = " + res )
          }
+      }
+   }
+
+   /**
+    * Tests the `map` method of `FutureResult`.
+    */
+   def future3() {
+      import actors.{TIMEOUT, Actor}
+      import Actor._
+
+      actor {
+         val ev = FutureResult.event[ Int ]()
+         val evP1 = ev.map( _ + 1 )
+         actor {
+            reactWithin( 3000L ) {
+               case TIMEOUT => ev.set( 33 )
+            }
+         }
+         println( "entering await" )
+         val res = evP1.apply()
+         println( "result = " + res )
+      }
+   }
+
+   /**
+    * Tests the `flatMap` method of `FutureResult`.
+    */
+   def future4() {
+      import actors.{TIMEOUT, Actor}
+      import Actor._
+
+      actor {
+         val ev = FutureResult.event[ Int ]()
+         val evm = ev.flatMap { i =>
+            val ev2 = FutureResult.event[ Int ]()
+            actor {
+               reactWithin( 3000L ) {
+                  case TIMEOUT =>
+                     println( "Setting ev2" )
+                     ev2.set( i * i )
+               }
+            }
+            ev2
+         }
+         actor {
+            reactWithin( 3000L ) {
+               case TIMEOUT =>
+                  println( "Setting ev" )
+                  ev.set( 33 )
+            }
+         }
+         println( "entering await" )
+         val res = evm.apply()
+         println( "result = " + res )
       }
    }
 }
