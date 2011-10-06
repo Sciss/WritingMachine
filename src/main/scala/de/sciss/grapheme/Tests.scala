7f8e50c4619f7b817d811d12bcbb1a9eedabac52
Sciss <contact@sciss.de>
2011-10-06 17:50:21 +0100

    ready to run findParts test -- hangs in filling, though

--- src/main/scala/de/sciss/grapheme/Tests.scala
+++ src/main/scala/de/sciss/grapheme/Tests.scala
@@ -1,3 +1,28 @@
+/*
+ *  Tests.scala
+ *  (WritingMachine)
+ *
+ *  Copyright (c) 2011 Hanns Holger Rutz. All rights reserved.
+ *
+ *  This software is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either
+ *  version 2, june 1991 of the License, or (at your option) any later version.
+ *
+ *  This software is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ *  General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public
+ *  License (gpl.txt) along with this software; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *
+ *  For further information, please contact Hanns Holger Rutz at
+ *  contact@sciss.de
+ */
+
 package de.sciss.grapheme
 
 import java.io.File
@@ -6,11 +31,12 @@ import collection.immutable.{IndexedSeq => IIdxSeq}
 object Tests {
    def main( args: Array[ String ]) {
       args.headOption match {
-         case Some( "--fut1" ) => future1()
-         case Some( "--fut2" ) => future2()
-         case Some( "--fut3" ) => future3()
-         case Some( "--fut4" ) => future4()
-         case Some( "--segm" ) => segm()
+         case Some( "--fut1" )   => future1()
+         case Some( "--fut2" )   => future2()
+         case Some( "--fut3" )   => future3()
+         case Some( "--fut4" )   => future4()
+         case Some( "--segm" )   => segm()
+         case Some( "--parts" )  => findParts()
          case _ => sys.error( "Illegal arguments : " + args )
       }
    }
@@ -28,7 +54,9 @@ object Tests {
       segmAndThen { ovs =>
          atomic( "check db" ) { tx1 =>
             val fillFut = fill.perform( tx1 )
-
+            println( "==== Waiting for Filling ====")
+            fillFut()
+            println( "==== Filling done ====" )
          }
       }
    }
--- src/main/scala/de/sciss/grapheme/impl/FileTelevisionImpl.scala
+++ src/main/scala/de/sciss/grapheme/impl/FileTelevisionImpl.scala
@@ -1,3 +1,28 @@
+/*
+ *  FileTelevisionImpl.scala
+ *  (WritingMachine)
+ *
+ *  Copyright (c) 2011 Hanns Holger Rutz. All rights reserved.
+ *
+ *  This software is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either
+ *  version 2, june 1991 of the License, or (at your option) any later version.
+ *
+ *  This software is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ *  General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public
+ *  License (gpl.txt) along with this software; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *
+ *  For further information, please contact Hanns Holger Rutz at
+ *  contact@sciss.de
+ */
+
 package de.sciss.grapheme
 package impl
 
@@ -15,11 +40,43 @@ object FileTelevisionImpl {
 class FileTelevisionImpl private ( f: File, spec: AudioFileSpec ) extends Television {
    import GraphemeUtil._
 
-   def capture( length: Long )( implicit tx: Tx ) : FutureResult[ File ] = {
-      threadFuture( "FileTelevisionImpl capture" ) {
-         val fNew = createTempFile( ".aif" )
+   private val posRef = Ref( 0L )
 
-         sys.error( "TODO" ) : File
+   def capture( length: Long )( implicit tx: Tx ) : FutureResult[ File ] = {
+      val oldPos = posRef()
+      threadFuture( "FileTelevisionImpl capture" ) {
+         try {
+            val fNew    = createTempFile( ".aif" )
+            val afNew   = openMonoWrite( fNew )
+            try {
+               val afTV = AudioFile.openRead( f )
+               try {
+                  var pos     = oldPos
+                  var left    = length
+                  afTV.seek( oldPos )
+                  while( left > 0L ) {
+                     val chunkLen = min( spec.numFrames - pos, left )
+                     afTV.copyTo( afNew, chunkLen )
+                     pos += chunkLen
+                     if( pos == spec.numFrames ) {
+                        afTV.seek( 0L )
+                        pos = 0L
+                     }
+                     left -= chunkLen
+                  }
+                  fNew
+               } finally {
+                  afTV.close()
+               }
+            } catch {
+               case e =>
+                  println( "FileTelevisionImpl capture - Oops. exception. Should handle" )
+                  e.printStackTrace()
+                  fNew // que puede... XXX
+            } finally {
+               afNew.close()
+            }
+         }
       }
    }
 }
